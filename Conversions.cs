22f7c61f848efb1f5f0ad76df1471a7fd86a833f
Егор Зубков <zegal2000@gmail.com>
2021-04-14 15:34:19 +0300

    Update_1

--- Conversions.cs
+++ Conversions.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Globalization;
+using System.Numerics;
 using System.Windows.Data;
 
 
 namespace labworkUI_1 {
     [ValueConversion(typeof(double), typeof(string))]
-    public class AvgToStr : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => $"{(double)value}";
+    public class MinMaxToStr : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => $"Minimum = {(((Complex, Complex))value).Item1}\tMaximum = {(((Complex, Complex))value).Item2}";
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
     }
 }
